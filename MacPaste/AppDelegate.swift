293190e4f57f0b61bfcd1431197d1eadd6d18e5b
AlonsoZhang <code@zhangwu.tech>
2018-05-14 11:26:47 +0800

    Basic function finish

--- MacPaste/AppDelegate.swift
+++ MacPaste/AppDelegate.swift
@@ -11,36 +11,68 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    @objc dynamic var Copys:[[String:String]] = []
+    @objc dynamic var Show = true
+    
     let hotkey = JFHotkeyManager.init()
     var timer : Timer!
     
     @IBOutlet weak var window: NSWindow!
-
-
+    @IBOutlet weak var tableview: NSTableView!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        //hotkey.bind("cmd c", target: self, action:#selector(AppDelegate.copyaction))
         timer=Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(AppDelegate.checkPasteboard), userInfo: nil, repeats: true)
-        hotkey.bind("cmd v", target: self, action: #selector(AppDelegate.pasteaction))
-    }
-
-    @objc func copyaction() {
-        print("copy")
+        hotkey.bind("control v", target: self, action: #selector(AppDelegate.showhide))
+        hotkey.bind("cmd q", target: self, action: #selector(AppDelegate.quit))
+        self.Copys = [["Copy":"Please copy your first string"]]
+        self.tableview.action = #selector(AppDelegate.singleaction)
+        //self.tableview.doubleAction = #selector(AppDelegate.doubleaction)
     }
     
-    @objc func pasteaction() {
-        print("paste")
+    @objc func showhide() {
+        if Show {
+            Show = false
+        }else{
+            Show = true
+            NSRunningApplication.current.activate(options: [.activateAllWindows,.activateIgnoringOtherApps])
+        }
     }
     
     @objc func checkPasteboard() {
-        //let pasteboard = NSPasteboard.general
-        if let ss =  NSPasteboard.general.string(forType:.string){
-             print(ss)
+        if let lastPasteboard =  NSPasteboard.general.string(forType:.string){
+            if Copys.count > 0
+            {
+                if lastPasteboard != Copys.first!["Copy"]{
+                    Copys.insert(["Copy":lastPasteboard], at: 0)
+                }
+            }else{
+                Copys.insert(["Copy":lastPasteboard], at: 0)
+            }
+            tableview.reloadData()
         }
+    }
+    
+    @objc func singleaction() {
+        let selectInt = self.tableview.selectedRow
+        if selectInt > -1 {
+            let copyinfo = Copys[selectInt]["Copy"]
+            Copys.remove(at: selectInt)
+            NSPasteboard.general.clearContents()
+            NSPasteboard.general.writeObjects([copyinfo! as NSPasteboardWriting])
+            showhide()
+        }
+    }
+    
+    @objc func quit() {
+        NSApp.terminate(self)
     }
     
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return false
+    }
 }
 
